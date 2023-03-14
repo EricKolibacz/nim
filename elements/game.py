aaa7c941bcb70242d140f2c9502830b54cb63225
EricKolibacz <26793186+EricKolibacz@users.noreply.github.com>
2023-03-14 17:14:22 +0100

    🏗️ Change illegal move policy to abort
    - instead of loosing instantly

--- elements/game.py
+++ elements/game.py
@@ -13,8 +13,6 @@ def play_game(players: Player, board: Board, verbosity: int = 0) -> List[Player]
     current_player = random.choice(players)
     current_player.is_starting()
 
-    has_lost_of_illegal_move: Player = Random("ReallyRandomy")
-
     while not game.has_ended():
         if verbosity >= 2:
             print(f"It is {current_player.name}'s move.")
@@ -26,20 +24,14 @@ def play_game(players: Player, board: Board, verbosity: int = 0) -> List[Player]
                 print(f"Taking {action.no_of_stones} stones from pile {action.pile}")
             game.remove(action)
         except ValueError as error_message:
-            if "NIM_ERROR:" in error_message.args[0]:
-                current_player = players[1] if current_player == players[0] else players[0]
-                not_current_player = players[1] if current_player == players[0] else players[0]
-                has_lost_of_illegal_move = not_current_player
-                break
-            else:
-                print("Encountered Error: ", error_message)
+            print("Encountered Error: ", error_message)
             input()
         else:
             current_player = players[1] if current_player == players[0] else players[0]
             if verbosity >= 2:
                 print()
     for player in players:
-        player.evaluate_result(current_player == player, has_lost_of_illegal_move == player)
+        player.evaluate_result(current_player == player)
     if verbosity >= 2:
         print(f"{current_player.name} won.")
 
